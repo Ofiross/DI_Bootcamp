d31a7a24dbd9e2abfb80107c4ce8196305ae1805
Ofiross <ofir23851@gmail.com>
2022-01-18 21:38:26 +0100

    day2 daily challenge update

--- week4/day2/dailyChallenge/script/script.js
+++ week4/day2/dailyChallenge/script/script.js
@@ -1,26 +1,25 @@
 /*--------------Daily Challenge------------*/
 let starsAround = () => {
-    let str = prompt("Please insert a sentence with comma between each word")
-    let wordLength = str.split(',').map(item => '' + item.trim());
+    let str = prompt("Please insert a sentence with comma between each word").split(',')
+        .map(item => '' + item
+            .trim());
+    /*     let wordLength = str.split(',')
+            .map(item => '' + item
+                .trim()); */
     let longestWord = 0;
-    let string = ""
-    for (let i = 0; i < wordLength.length; i++) {
-        if (wordLength[i].length > longestWord) {
-            longestWord = wordLength[i].length + 4
-        }
-        for (let j = 0; j < wordLength.length; j++) {
-            if (wordLength[j].length < longestWord) {
-                string += " "
-            } else {
-                string = ""
-            }
-
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].length > longestWord) {
+            longestWord = str[i].length
         }
     }
-    let arrayFromString = str.split(',').map(item => '* ' + item.trim() + (`${string} *`));
-    let star = "*"
-    let stars = star.repeat(longestWord)
-    arrayFromString.push(stars)
-    arrayFromString.splice(0, 0, stars)
-    console.log(arrayFromString.join(`\n`))
+    let stars = "*".repeat(longestWord + 4)
+    str = str.map(wd => { return wd + (new Array(longestWord - wd.length).fill(' ').join('')) });
+
+
+    let newStr = str.map(w => '* ' + w + ' *');
+    newStr.push(stars)
+    newStr.splice(0, 0, stars)
+
+
+    console.log(newStr.join(`\n`))
 }
