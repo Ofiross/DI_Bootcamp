541b7a7ab6da78fcccebf0b6a3ec48b8a1e0ad24
Ofiross <ofir23851@gmail.com>
2022-02-28 13:06:59 +0100

    28/02/2022

--- week10/day2/Daily_Challenge/Daily_Challenge.py
+++ week10/day2/Daily_Challenge/Daily_Challenge.py
@@ -1,4 +1,19 @@
 # Daily Challenge
+from translate import Translator
 
 
+translator = Translator(to_lang="en", from_lang="fr")
 french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+
+
+def Convert(lst):
+    """Converting a list of words from french into english"""
+    translated_words = []
+    for i in lst:
+        translated_words.append(translator.translate(i))
+        result_zipped = zip(lst, translated_words)
+        result_dict = dict(result_zipped)
+    print(f"\n{'-' *80}\nThe translation from French to English is:\n{result_dict}\n{'-' *80}\n")
+
+
+Convert(french_words)
