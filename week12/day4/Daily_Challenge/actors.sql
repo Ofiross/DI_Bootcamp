623523a824de17f8a217e4075f69c13e3eab6a4e
Ofiross <ofir23851@gmail.com>
2022-03-16 12:50:44 +0100

    16/03/22

---
+++ week12/day4/Daily_Challenge/actors.sql
@@ -0,0 +1,4 @@
+-- SELECT COUNT(*) AS NumberOfActors FROM actors
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Matthew','McConaughey', , 5);
---
+++ week12/day4/Exercise_XP/Exercise_XP.sql
@@ -0,0 +1,35 @@
+-- CREATE TABLE items (
+-- item_id SERIAL PRIMARY KEY,
+-- desk_type VARCHAR (15) NOT NULL,
+-- desk_price SMALLINT NOT NULL
+-- )
+
+-- INSERT INTO items (desk_type, desk_price)
+-- VALUES('Small Desk','100'), ('Large desk','300'), ('Fan','80');
+
+-- INSERT INTO items (desk_type, desk_price)
+-- VALUES('Small Desk','100'), ('Large desk','300'), ('Fan','80');
+
+-- CREATE TABLE customers (
+-- customers_id SERIAL PRIMARY KEY,
+-- customer_name VARCHAR (15) NOT NULL,
+-- customer_surname VARCHAR (15) NULL
+-- )
+
+-- SELECT
+--    desk_type
+-- FROM
+--    items;
+
+-- SELECT * FROM items WHERE desk_price > 80;
+
+-- INSERT INTO customers (customer_name, customer_surname)
+-- VALUES('Greg','Jones'), ('Sandra','Jones'), ('Scott','Scott'), ('Trevor','Green'), ('Melanie','Johnson');
+
+--SELECT * FROM customers WHERE customer_surname = 'Smith';
+
+-- SELECT * FROM customers WHERE customer_surname != 'Scott';
+
+
+
+
